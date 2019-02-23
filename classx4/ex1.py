36d29ab142e2d2a13f2278ba224f9e00a206f1a2
Wayneplp <2310839613@qq.com>
2019-02-23 09:08:47 +0800

    贪吃蛇前期

---
+++ classx4/ex1.py
@@ -0,0 +1,70 @@
+import pygame
+from pygame.locals import *
+RED = (255,0,0)
+BLACK = (0,0,0)
+keepGoing = True  
+SNAKEBODY = 20
+SNAKECOLOR = RED
+xs = [300,300,300,300,300]
+ys = [280,260,240,220,200]
+SCREENWIDTH = 1000
+SCREENHEIGHT = 1000
+img = pygame.Surface((SNEAKBODY,SNAKECOLOR))
+img = fill(SNEAKBODY)
+dire = "DOWN"
+pygame.init()
+screen = pygame.dispaly.set_mode((SCREENWIDTH,SCREENHEIGHT))
+xspeend = 1
+yspeend = 1
+while keepGoing:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			keepGoing = False
+		elif event.key == KEYDOWN:
+		    if event.key == K_UP and dire != "DOWN":
+		    	dire = "UP"
+		    	yspeend +=1
+		    elif event.key == K_DOWN and dire != "UP":
+		    	dire = "DOWN"
+		    	yspeend -=1
+		    elif event.key == K_RIGHT and dire != "LEFT":
+		        dire = "RIGHT"
+		        xspeend -=1
+		    elif event.key == K_LEFT and dire != "RIGHT":
+		    	dire = "LEFT"
+		    	xspeend +=1
+	
+    # draw snakebody
+	i = len(xs) -1
+	while i>=1:
+		xs[i] = xs[i-1]
+		ys[i] = ys[i-1]
+		i -=1
+	
+    #draw snakehead 
+	if dire == 'UP':
+		ys[0] = ys[0] + SNAKEBODY
+	elif dire == 'DOWN':
+		ys[0] = ys[0] - SNAKEBODY
+	elif dire == 'LEFT':
+		xs[0] = xs[0]- SNAKEBODY
+	elif dire == 'RIGHT':
+		xs[0] = xs[0] - SNAKEBODY
+
+    #print snake
+    screen.fill(BLACK)
+    for i in range(0,len(xs)):
+    	screen.bulit(img,(xs[i],ys[i]))
+
+
+    	pygame.display.update()
+
+pygame.quit()
+
+
+
+		
+
+
+
+
